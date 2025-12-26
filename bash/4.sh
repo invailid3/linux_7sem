95b67abc2035d97e58a104ad8c1ea56f67ef0ef4
invailid3 <artemsanakan2@gmail.com>
2025-12-26 16:16:15 +0000

    выполнила задания по башу

--- bash/4.sh
+++ bash/4.sh
@@ -1,2 +1,32 @@
 #!/bin/bash
 
+planet="$1"
+
+case $planet in
+"Mercury")
+	echo "0"
+	;;
+"Venus")
+	echo "0"
+	;;
+"Mars")
+	echo "2"
+	;;
+"Jupiter")
+	echo "98"
+	;;
+"Saturn")
+	echo "83"
+	;;
+"Uranus")
+	echo "27"
+	;;
+"Neptune")
+	echo "14"
+	;;
+"Earth")
+	echo "1"
+	;;
+esac
+
+echo "Unknown planet"
--- bash/6.sh
+++ bash/6.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
 
+set -euo pipefail
+old_files=$(ls -A)
+
+if [ "${BAR:-}" = "1" ]; then
+	exit 1234
+fi
+
+if [ -f "./fix.txt" ]; then
+	echo "file detected";
+	exit
+fi
+
+while true; do
+	cur_files=$(ls -A)
+	if [[ "$cur" != "$old_files" ]]; then
+		echo "operation detected";
+		break
+	fi
+done
